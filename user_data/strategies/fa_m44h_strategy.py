f61a4dcba9107600deb15018c5952517f015d62c
Dean Martin <dean.martin@frostaura.net>
2021-04-22 18:51:51 +0200

    [DEV]: Adding of samples and M4 strategy.

---
+++ user_data/strategies/fa_m44h_strategy.py
@@ -0,0 +1,115 @@
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+from freqtrade.strategy.interface import IStrategy
+import talib.abstract as ta
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+
+class FrostAuraM44hStrategy(IStrategy):
+    """
+    This is FrostAura's mark 4 stretagy with RSI and MACD.
+    
+    Last Optimization:
+        Profit %        : 1203.39%
+        Optimized for   : Last 60 days, 4h
+        Avg             : 5524.0m
+    """
+    # Strategy interface version - allow new iterations of the strategy interface.
+    # Check the documentation or the Sample strategy to get the latest version.
+    INTERFACE_VERSION = 2
+
+    # Minimal ROI designed for the strategy.
+    minimal_roi = {
+        "0": 0.28109,
+        "1785": 0.19465,
+        "3917": 0.03538,
+        "8147": 0
+    }
+
+    # Optimal stoploss designed for the strategy.
+    stoploss = -0.28561
+
+    # Trailing stoploss
+    trailing_stop = False
+
+    # Optimal ticker interval for the strategy.
+    timeframe = '4h'
+
+    # Run "populate_indicators()" only for new candle.
+    process_only_new_candles = False
+
+    # These values can be overridden in the "ask_strategy" section in the config.
+    use_sell_signal = True
+    sell_profit_only = False
+    ignore_roi_if_buy_signal = False
+
+    # Number of candles the strategy requires before producing valid signals.
+    startup_candle_count: int = 30
+
+    # Optional order type mapping.
+    order_types = {
+        'buy': 'limit',
+        'sell': 'limit',
+        'stoploss': 'market',
+        'stoploss_on_exchange': False
+    }
+
+    # Optional order time in force.
+    order_time_in_force = {
+        'buy': 'gtc',
+        'sell': 'gtc'
+    }
+
+    plot_config = {
+        'main_plot': {
+            'tema': {},
+            'sar': {'color': 'white'},
+        },
+        'subplots': {
+            "MACD": {
+                'macd': {'color': 'blue'},
+                'macdsignal': {'color': 'orange'},
+            },
+            "RSI": {
+                'rsi': {'color': 'red'},
+            }
+        }
+    }
+
+    def informative_pairs(self):
+        return []
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        # RSI
+        dataframe['rsi'] = ta.RSI(dataframe)
+        
+        # MACD
+        macd = ta.MACD(dataframe)
+        dataframe['macd'] = macd['macd']
+        dataframe['macdsignal'] = macd['macdsignal']
+        dataframe['macdhist'] = macd['macdhist']
+
+        return dataframe
+
+    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        minimum_coin_price = 0.0000015
+        
+        dataframe.loc[
+            (
+                (dataframe['rsi'] > 30) &
+                (dataframe['macd'] > dataframe['macdsignal']) &
+                (dataframe["close"] > minimum_coin_price)
+            ),
+            'buy'] = 1
+
+        return dataframe
+
+    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe.loc[
+            (
+                (dataframe['rsi'] < 51) &
+                (dataframe['macd'] > dataframe['macdsignal'])
+            ),
+            'sell'] = 1
+        
+        return dataframe

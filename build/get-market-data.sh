5938cab4e306bfbe7af43605b53db424a827a702
Dean Martin <dean.martin@frostaura.net>
2021-05-28 14:07:36 +0200

    [DEV]: Updating build definition.

--- build/get-market-data.sh
+++ build/get-market-data.sh
@@ -1,2 +1,14 @@
+!/bin/sh
+
+while getopts e:d: flag
+do
+    case "${flag}" in
+        e) epochs=${OPTARG};;
+        # Should be '../user_data/strategies' for local testing.
+        d) stratDir=${OPTARG};;
+    esac
+done
+
 echo "Getting latest market data."
 docker-compose run --rm freqtrade download-data --exchange binance --days 30 -t 1h
+./optimize-strategies.sh -e $epochs -d $stratDir

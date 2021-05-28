22ea28e7fa4925d082d4d5d3f37b2a55de8b913a
Dean Martin <dean.martin@frostaura.net>
2021-05-28 14:14:34 +0200

    [DEV]: More updates.

--- build/get-market-data.sh
+++ build/get-market-data.sh
@@ -11,4 +11,4 @@ done
 
 echo "Getting latest market data."
 docker-compose run --rm freqtrade download-data --exchange binance --days 30 -t 1h
-./optimize-strategies.sh -e $epochs -d $stratDir
+./build/optimize-strategies.sh -e $epochs -d $stratDir

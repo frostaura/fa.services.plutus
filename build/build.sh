d50a50b2b8fefc6fa6407fe3069b4b8248b2c5d3
Automation Pipeline <automation@frostaura.net>
2021-06-08 17:04:17 +0200

    [DEV]: Automating comitting of optimizations take 1.

--- build/build.sh
+++ build/build.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-while getopts e:d: flag
+while getopts e:d:t: flag
 do
     case "${flag}" in
         e) epochs=${OPTARG};;
         d) baseDir=${OPTARG};;
+        t) token=${OPTARG};;
     esac
 done
 
 echo "[BUILD]"
 eval "${baseDir}/build/get-market-data.sh -e $epochs -d \"$baseDir\""
 eval "${baseDir}/build/optimize-strategies.sh -e $epochs -d \"$baseDir\""
-eval "${baseDir}/build/commit-optimizations.sh -d \"$baseDir\""
+eval "${baseDir}/build/commit-optimizations.sh -d \"$baseDir\" -t \"$token\""
--- build/commit-optimizations.sh
+++ build/commit-optimizations.sh
@@ -1,10 +1,34 @@
 #!/usr/bin/env bash
 
-while getopts d: flag
+while getopts d:t: flag
 do
     case "${flag}" in
         d) baseDir=${OPTARG};;
+        t) token=${OPTARG};;
     esac
 done
 
 echo "[OPTIMIZE][STRATEGIES][COMMITTING]"
+
+setup() {
+    git config --global user.email "automation@frostaura.net"
+    git config --global user.name "Automation Pipeline"
+}
+
+commit() {
+    git checkout main
+    git add -f "${baseDir}/automation/optimizations/*"
+    git commit -m "Automated strategy optimizations."
+}
+
+push() {
+    dateTime="optimizations-$(date +%s)"
+    git remote rm origin
+    git remote add origin "https://${token}@github.com/faGH/fa.services.plutus.git" > /dev/null 2>&1
+    git tag -a $dateTime -m "Automated optimizations."
+    git push origin main --quiet --tags $dateTime
+}
+
+setup
+commit
+push

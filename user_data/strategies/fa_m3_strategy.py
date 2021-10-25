9a0389586e3639e5262a61226faf17eab8589efe
Dean Martin <dean.martin@frostaura.net>
2021-10-25 20:19:19 +0200

    [DEV]: Ported and optimized M3.

--- user_data/strategies/fa_m3_strategy.py
+++ user_data/strategies/fa_m3_strategy.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
-from freqtrade.strategy.interface import IStrategy
+from freqtrade.strategy import (IntParameter, IStrategy, CategoricalParameter)
 import talib.abstract as ta
 import freqtrade.vendor.qtpylib.indicators as qtpylib
 
@@ -11,10 +11,9 @@ class FrostAuraM3Strategy(IStrategy):
     based on the BB, RSI and Stochastic.
     
     Last Optimization:
-        Profit %        : 67.58%
-        Optimized for   : Last 30 days, 1h
-        Avg             : 811.0 m
-        Obj             : -1.02323
+        Profit %        : 7.04%
+        Optimized for   : Last 45 days, 1h
+        Avg             : 2d 5h 20m
     """
     # Strategy interface version - allow new iterations of the strategy interface.
     # Check the documentation or the Sample strategy to get the latest version.
@@ -22,14 +21,20 @@ class FrostAuraM3Strategy(IStrategy):
 
     # Minimal ROI designed for the strategy.
     minimal_roi = {
-        "0": 0.09466,
-        "128": 0.0675,
-        "262": 0.03632,
-        "777": 0
+        "0": 0.186,
+        "226": 0.145,
+        "809": 0.085,
+        "2235": 0
     }
 
-    # Optimal stoploss designed for the strategy.
-    stoploss = -0.16647
+    # Stoploss:
+    stoploss = -0.303
+
+    # Trailing stop:
+    trailing_stop = False  # value loaded from strategy
+    trailing_stop_positive = None  # value loaded from strategy
+    trailing_stop_positive_offset = 0.0  # value loaded from strategy
+    trailing_only_offset_is_reached = False  # value loaded from strategy
 
     # Trailing stoploss
     trailing_stop = False
@@ -113,14 +118,20 @@ class FrostAuraM3Strategy(IStrategy):
 
         return dataframe
 
+    buy_rsi = IntParameter([20, 80], default=20, space='buy')
+    buy_band = CategoricalParameter(['lower', 'middle', 'upper'], default='lower', space='buy')
+    buy_std = CategoricalParameter(['1', '2', '3', '4'], default='2', space='buy')
+    buy_slowd = IntParameter([20, 80], default=23, space='buy')
+    buy_slowk = IntParameter([20, 80], default=22, space='buy')
+
     def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         minimum_coin_price = 0.0000015
-        var_buy_rsi = 45
-        var_buy_band = 'lower'
-        var_buy_std = '1'
+        var_buy_rsi = self.buy_rsi.value
+        var_buy_band = self.buy_band.value
+        var_buy_std = self.buy_std.value
         var_buy_band_value = dataframe['bb_' + var_buy_band + 'band' + var_buy_std]
-        var_buy_slowd = 28
-        var_buy_slowk = 73
+        var_buy_slowd = self.buy_slowd.value
+        var_buy_slowk = self.buy_slowk.value
         
         dataframe.loc[
             (
@@ -134,13 +145,19 @@ class FrostAuraM3Strategy(IStrategy):
 
         return dataframe
 
+    sell_rsi = IntParameter([20, 80], default=77, space='sell')
+    sell_band = CategoricalParameter(['lower', 'middle', 'upper'], default='lower', space='sell')
+    sell_std = CategoricalParameter(['1', '2', '3', '4'], default='4', space='sell')
+    sell_slowd = IntParameter([20, 80], default=30, space='sell')
+    sell_slowk = IntParameter([20, 80], default=61, space='sell')
+
     def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        var_sell_rsi = 48
-        var_sell_band = 'upper'
-        var_sell_std = '2'
+        var_sell_rsi = self.sell_rsi.value
+        var_sell_band = self.sell_band.value
+        var_sell_std = self.sell_std.value
         var_sell_band_value = dataframe['bb_' + var_sell_band + 'band' + var_sell_std]
-        var_sell_slowd = 36
-        var_sell_slowk = 26
+        var_sell_slowd = self.sell_slowd.value
+        var_sell_slowk = self.sell_slowk.value
         
         dataframe.loc[
             (
